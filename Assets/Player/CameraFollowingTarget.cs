cc5e7c597a8c4c0df6da7d91689f7ff5c8005640
hastgt <hastgt@icloud.com>
2019-04-27 23:17:26 +0200

    camerashake
    camera, movement

---
+++ Assets/Player/CameraFollowingTarget.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class CameraFollowingTarget : MonoBehaviour
+{
+    public Vector3 offset;
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public float cameraDistance = 40f;
+
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = transform.position = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+    public float speed;
+    private Rigidbody2D vampyr;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vampyr = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        vampyr.MovePosition(vampyr.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
